22b1cacca315cb262a6d5d8db6debd3f36ebef49
Ronnie Song <ronniesong0809@gmail.com>
2021-07-03 20:27:52 -0700

    add more question details to sms

--- twilio.js
+++ twilio.js
@@ -4,9 +4,13 @@ const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken);
 
 const sendSms = (question) => {
+  
+  let feq = parseFloat(question.frequency).toFixed(2);
+  let topics = question.topics.map(topic => topic.name).join(', ');
+
   client.messages
     .create({
-       body: `Today's question:\n\nLC ${question.id} - ${question.title}`,
+       body: `Today's question:\n\nLC ${question.id} - ${question.title}\n${question.lvl} - ${feq}\n${topics}\n${question.url}`,
        from: process.env.TWILIO_FROM_PHONE_NUMBER,
        to: process.env.TWILIO_TO_PHONE_NUMBER
      })
